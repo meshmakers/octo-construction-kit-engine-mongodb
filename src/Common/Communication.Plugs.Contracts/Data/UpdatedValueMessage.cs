91471238347b940240ab220d4bcd92d75a0c4a61
Gerald Lochner <gerald.lochner@meshmakers.io>
2023-05-18 13:25:52 +0200

    Fixing issues in nullable types

--- src/Common/Communication.Plugs.Contracts/Data/UpdatedValueMessage.cs
+++ src/Common/Communication.Plugs.Contracts/Data/UpdatedValueMessage.cs
@@ -2,9 +2,9 @@ namespace Meshmakers.Octo.Communication.Plugs.Contracts.Data;
 
 public class UpdatedValueMessage
 {
-    public string PlugId { get; set; }
-    public string Group { get; set; }
-    public string Name { get; set; }
+    public string PlugId { get; set; } = null!;
+    public string Group { get; set; } = null!;
+    public string Name { get; set; } = null!;
     public object? Value { get; set; }
     public DateTime PlugReceivedDateTime { get; set; }
     public DateTime? ExternalReceivedDateTime { get; set; }
--- src/Common/Communication.Plugs.Contracts/Data/UpdatedValueMessages.cs
+++ src/Common/Communication.Plugs.Contracts/Data/UpdatedValueMessages.cs
@@ -2,5 +2,5 @@ namespace Meshmakers.Octo.Communication.Plugs.Contracts.Data;
 
 public class UpdatedValueMessages
 {
-    private IEnumerable<UpdatedValueMessage> List { get; set; }
+    private IEnumerable<UpdatedValueMessage> List { get; set; } = null!;
 }
