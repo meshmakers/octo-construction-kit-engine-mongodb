5a27b34bbeed13eb5eb08389b717c9c73e310b72
Gerald Lochner <gerald.lochner@meshmakers.io>
2023-05-18 13:04:13 +0200

    Added dtos for messages

---
+++ src/Common/Communication.Plugs.Contracts/Data/UpdatedValueMessage.cs
@@ -0,0 +1,11 @@
+namespace Meshmakers.Octo.Communication.Plugs.Contracts.Data;
+
+public class UpdatedValueMessage
+{
+    public string PlugId { get; set; }
+    public string Group { get; set; }
+    public string Name { get; set; }
+    public object Value { get; set; }
+    public DateTime PlugReceivedDateTime { get; set; }
+    public DateTime? ExternalReceivedDateTime { get; set; }
+}
---
+++ src/Common/Communication.Plugs.Contracts/Data/UpdatedValueMessages.cs
@@ -0,0 +1,6 @@
+namespace Meshmakers.Octo.Communication.Plugs.Contracts.Data;
+
+public class UpdatedValueMessages
+{
+    private IEnumerable<UpdatedValueMessage> List { get; set; }
+}
