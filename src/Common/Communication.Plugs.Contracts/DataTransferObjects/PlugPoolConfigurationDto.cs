ecd810d13da7c7c33fb698951b16f3e26f4ef2d7
Gerald Lochner <gerald.lochner@meshmakers.io>
2023-05-25 09:22:14 +0200

    Added definitions

--- src/Common/Communication.Plugs.Contracts/DataTransferObjects/PlugPoolConfigurationDto.cs
+++ src/Common/Communication.Plugs.Contracts/DataTransferObjects/PlugPoolConfigurationDto.cs
@@ -2,5 +2,9 @@ namespace Meshmakers.Octo.Communication.Plugs.Contracts.DataTransferObjects;
 
 public record PlugPoolConfigurationDto
 {
-    public IEnumerable<PlugPoolPlug> Plugs { get; set; } = null!;
+    public IEnumerable<PlugPoolPlugDto> Plugs { get; set; } = null!;
+    
+    public string BrokerHost { get; set; } = string.Empty;
+    public string BrokerVirtualHost { get; set; } = string.Empty;
+    public ushort BrokerPort { get; set; } = 5672;
 }
--- src/Common/Communication.Plugs.Contracts/DataTransferObjects/PlugPoolPlugDto.cs
+++ src/Common/Communication.Plugs.Contracts/DataTransferObjects/PlugPoolPlugDto.cs
@@ -2,8 +2,9 @@ using Meshmakers.Octo.Common.Shared;
 
 namespace Meshmakers.Octo.Communication.Plugs.Contracts.DataTransferObjects;
 
-public record PlugPoolPlug
+public record PlugPoolPlugDto
 {
     public OctoObjectId PlugId { get; set; }
+    public string ImageName { get; set; }
     public string Version { get; set; }
 }
