5028de46230a4c73236a5f526d44a958aa017b31
Gerald Lochner <gerald.lochner@meshmakers.io>
2023-05-30 10:42:39 +0200

    Fixing dtos

--- src/Common/Communication.Plugs.Contracts/DataTransferObjects/PlugPoolPlugDto.cs
+++ src/Common/Communication.Plugs.Contracts/DataTransferObjects/PlugPoolPlugDto.cs
@@ -1,10 +1,12 @@
 using Meshmakers.Octo.Common.Shared;
+// ReSharper disable ClassNeverInstantiated.Global
 
 namespace Meshmakers.Octo.Communication.Plugs.Contracts.DataTransferObjects;
 
 public record PlugPoolPlugDto
 {
+    public string PlugPoolName { get; set; } = null!;
     public OctoObjectId PlugId { get; set; }
-    public string ImageName { get; set; }
-    public string Version { get; set; }
+    public string ImageName { get; set; } = null!;
+    public string Version { get; set; } = null!;
 }
