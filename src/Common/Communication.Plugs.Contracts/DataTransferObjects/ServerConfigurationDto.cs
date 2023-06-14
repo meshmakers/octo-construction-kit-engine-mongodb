7df4e27242c7746647e3130967c5e849f966bf27
Gerald Lochner <gerald.lochner@meshmakers.io>
2023-06-14 16:04:53 +0200

    Fixes

--- src/Common/Communication.Plugs.Contracts/DataTransferObjects/ServerConfigurationDto.cs
+++ src/Common/Communication.Plugs.Contracts/DataTransferObjects/ServerConfigurationDto.cs
@@ -3,7 +3,7 @@ namespace Meshmakers.Octo.Communication.Plugs.Contracts.DataTransferObjects;
 public record ServerConfigurationDto
 {
     public string Server { get; set; } = null!;
-    
+
     public IReadOnlyCollection<GroupConfigurationDto> Groups { get; set; } = null!;
 
     public virtual bool Equals(ServerConfigurationDto? other)
