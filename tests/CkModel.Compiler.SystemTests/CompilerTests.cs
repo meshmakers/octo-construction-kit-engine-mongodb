cb044f718140e8d8339c09f7eebb24a0d18a4e88
Gerald Lochner <gerald.lochner@meshmakers.io>
2023-08-18 13:17:20 +0200

    Naming

---
+++ tests/CkModel.Compiler.SystemTests/CompilerTests.cs
@@ -0,0 +1,30 @@
+using CkModel.Compiler.SystemTests.Fixtures;
+using Meshmakers.Octo.SystematizedData.CkModel.Compiler;
+using Meshmakers.Octo.SystematizedData.CkModel.Compiler.Serialization;
+
+namespace CkModel.Compiler.SystemTests;
+
+public class CompilerTests : IClassFixture<TemporaryDirectoryFixture>
+{
+    private readonly TemporaryDirectoryFixture _fixture;
+
+    public CompilerTests(TemporaryDirectoryFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public async Task CreateNew()
+    {
+        // Access _fixture.TempDirectoryPath to work with the temporary directory
+        string filePath = Path.Combine(_fixture.TempDirectoryPath, "CreateNew");
+        CompilerService compilerService = new CompilerService(new CkYamlSerializer());
+        await compilerService.CreateNewAsync(filePath);
+
+        Assert.True(Directory.Exists(filePath));
+        Assert.True(File.Exists(Path.Combine(filePath, CompilerStatics.MetadataFile)));
+        Assert.True(Directory.Exists(Path.Combine(filePath, CompilerStatics.TypesDirectoryName)));
+        Assert.True(Directory.Exists(Path.Combine(filePath, CompilerStatics.AssociationsDirectoryName)));
+        Assert.True(Directory.Exists(Path.Combine(filePath, CompilerStatics.AttributesDirectoryName)));
+    }
+}
