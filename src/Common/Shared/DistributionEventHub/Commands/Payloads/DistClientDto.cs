7fa0326f3e8f05777c31cbd5023bcfc0102ca739
Gerald Lochner <gerald.lochner@meshmakers.io>
2023-12-21 12:20:24 +0100

    Fixes

--- src/Common/Shared/DistributionEventHub/Commands/Payloads/DistClientDto.cs
+++ src/Common/Shared/DistributionEventHub/Commands/Payloads/DistClientDto.cs
@@ -12,7 +12,7 @@ public record DistClientDto(string ClientId, string ClientName, string ClientUri
     /// Gets or sets allowed grant types.
     /// </summary>
     public string[] AllowedGrantTypes { get; init; } = null!;
-    
+
     /// <summary>
     /// Gets or sets if a consent is required.
     /// </summary>
@@ -21,22 +21,22 @@ public record DistClientDto(string ClientId, string ClientName, string ClientUri
     /// <summary>
     /// Gets or sets redirect uris.
     /// </summary>
-    public ICollection<string> RedirectUris { get; } = new List<string>();
-    
+    public string[] RedirectUris { get; init; } = null!;
+
     /// <summary>
     /// Gets or sets post logout redirect uris.
     /// </summary>
-    public ICollection<string> PostLogoutRedirectUris { get; } = new List<string>();
-    
+    public string[] PostLogoutRedirectUris { get; init; } = null!;
+
     /// <summary>
     /// Gets or sets allowed cors origins.
     /// </summary>
-    public ICollection<string> AllowedCorsOrigins { get; } = new List<string>();
-    
+    public string[] AllowedCorsOrigins { get; init; } = null!;
+
     /// <summary>
     /// Gets or sets allowed scopes.
     /// </summary>
-    public ICollection<string> AllowedScopes { get; } = new List<string>();
+    public string[] AllowedScopes { get; init; } = null!;
 
     /// <summary>
     /// Gets or sets if offline access is allowed.
