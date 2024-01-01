cb83ebbc71aa77c7c3c2febd0ba44bab332e6353
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-01-01 18:17:29 +0100

    Fix build

--- tests/Persistence.SystemTests/Usings.cs
+++ tests/Persistence.SystemTests/Usings.cs
@@ -1,2 +1,2 @@
-global using Persistence.SystemCkModel.ConstructionKit.Generated.System.v1;
+global using Meshmakers.Octo.ConstructionKit.System.ConstructionKit.Generated.System.v1;
 global using Meshmakers.Octo.Runtime.Contracts.RepositoryEntities;
