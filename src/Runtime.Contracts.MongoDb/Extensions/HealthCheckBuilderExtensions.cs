76a5d3b72bafba8876b3e9a1234988d7e1c08d44
Reimar Klammer <reimar.klammer@meshmakers.io>
2024-07-01 13:17:55 +0200

    AB#1366 Add observability to Services

---
+++ src/Runtime.Contracts.MongoDb/Extensions/HealthCheckBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Meshmakers.Octo.Runtime.Contracts.MongoDb.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Meshmakers.Octo.Runtime.Contracts.MongoDb.Extensions;
+
+public static class HealthCheckBuilderExtensions
+{
+    private static readonly string[] MongodbTags = ["mongodb"];
+
+    public static IHealthChecksBuilder AddSystemContextHealthCheck(this IHealthChecksBuilder builder)
+    {
+        builder.AddCheck<TenantContextHealthCheck>(
+            "TenantContext", 
+            HealthStatus.Unhealthy, 
+            MongodbTags);
+        return builder;
+    }
+}
---
+++ src/Runtime.Contracts.MongoDb/Services/TenantContextHealthCheck.cs
@@ -0,0 +1,20 @@
+﻿using Meshmakers.Octo.ConstructionKit.Contracts;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Meshmakers.Octo.Runtime.Contracts.MongoDb.Services;
+
+public class TenantContextHealthCheck(ISystemContext tenantContext) : IHealthCheck
+{
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+    {
+        try
+        {
+            _ = await tenantContext.IsCkModelExistingAsync(new CkModelId("System", "1.0.0"));
+            return HealthCheckResult.Healthy("Mongodb is accessible.");
+        }
+        catch (Exception)
+        {
+            return HealthCheckResult.Unhealthy("Error while accessing mongodb.");
+        }
+    }
+}
