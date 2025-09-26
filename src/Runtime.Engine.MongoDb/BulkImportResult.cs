b642b7b1bed053ac3151334402dcdb28677e7105
Markus Roider <markus.roider@meshmakers.io>
2025-09-26 11:36:44 +0200

    AB#2663: Fix upsert logic for RtAssociations

--- src/Runtime.Engine.MongoDb/BulkImportResult.cs
+++ src/Runtime.Engine.MongoDb/BulkImportResult.cs
@@ -3,14 +3,14 @@ using MongoDB.Driver;
 
 namespace Meshmakers.Octo.Runtime.Engine.MongoDb;
 
-public class BulkImportResult(BulkWriteResult bulkWriteResult) : IBulkImportResult
+public class BulkImportResult(BulkWriteResult? bulkWriteResult) : IBulkImportResult
 {
-    public long InsertedCount => bulkWriteResult.InsertedCount;
-    public long DeletedCount => bulkWriteResult.DeletedCount;
-    public long ModifiedCount => bulkWriteResult.ModifiedCount;
+    public long InsertedCount => bulkWriteResult?.InsertedCount ?? 0;
+    public long DeletedCount => bulkWriteResult?.DeletedCount ?? 0;
+    public long ModifiedCount => bulkWriteResult?.ModifiedCount ?? 0;
 
     public bool HasError()
     {
-        return !bulkWriteResult.IsAcknowledged;
+        return (!bulkWriteResult?.IsAcknowledged) ?? false;
     }
 }
